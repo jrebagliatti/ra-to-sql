6a304d9c88b4ef8b8b303bfbc40fedd17c94a503
jrebagliatti <javier.rebagliatti@southworks.com>
2018-09-16 12:03:10 -0300

    Support last expression to be an assignment

--- src/ra-to-sql.js
+++ src/ra-to-sql.js
@@ -19,6 +19,8 @@ function getSql(ra) {
                 fieldList = `SELECT ${element.value.fields.map(x => `null as ${x}`).join(',')} WHERE 1=2 UNION ${fieldList}`;
             }
 
+            var newExpression = `(${fieldList} FROM ${element.value.expression})`;
+
             for(j = i + 1; j < result.length; j++){
                 var elementToReplace = result[j];
                 var valueToBeReplaced;
@@ -31,7 +33,7 @@ function getSql(ra) {
                 }
 
                 console.log(`  Inspecting [${elementToReplace.type}] ${valueToBeReplaced}`);
-                valueToBeReplaced = valueToBeReplaced.replace(new RegExp("\\(SELECT DISTINCT \\* FROM " + element.value.id + "\\)", 'g'), `(${fieldList} FROM ${element.value.expression})`);
+                valueToBeReplaced = valueToBeReplaced.replace(new RegExp("\\(SELECT DISTINCT \\* FROM " + element.value.id + "\\)", 'g'), newExpression);
 
                 if (elementToReplace.type == "identifier") {
                     elementToReplace.value.expression = valueToBeReplaced;
@@ -47,7 +49,9 @@ function getSql(ra) {
         }
     }
 
-    return `SELECT DISTINCT * FROM ${element.value.value}`;
+    var result = element.value.value || newExpression;
+
+    return `SELECT DISTINCT * FROM ${result}`;
 }
 
 module.exports = {
